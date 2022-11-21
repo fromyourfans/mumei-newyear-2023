0f9d4d4f2f6eb6d7a99ba4b001191b18da5865fd
dragonjet <jetriconew@gmail.com>
2022-11-21 13:54:48 -0800

    Initial design

---
+++ src/components/MessageCard.vue
@@ -0,0 +1,57 @@
+<script setup>
+defineProps({
+  info: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="card" >
+    <div class="card-img" :style="{ transform: `rotate(${Math.ceil(Math.random() * 10) - 5}deg)` }"></div>
+    <div class="card-name">{{info.name}}</div>
+    <div class="card-text">{{info.message}}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  flex-grow:1;
+  padding:80px 30px;
+  width: 360px;
+  height: 320px;
+  margin: 0px 5px 20px 5px;
+  position:relative;
+  .card-img {
+    width: 100%;
+    height: 320px;
+    background-image: url('../assets/card.png');
+    background-repeat: no-repeat;
+    background-position: top center;
+    background-size: 360px 320px;
+    position:absolute;
+    z-index:1;
+    top:0;
+    left:0;
+  }
+  .card-name {
+    color:#000000;
+    font-weight:bold;
+    text-align:center;
+    font-size:18px;
+    margin-bottom:20px;
+    position:relative;
+    z-index:3;
+  }
+  .card-text {
+    color: #262626;
+    width:300px;
+    margin:0px auto;
+    font-size:16px;
+    line-height:20px;
+    position:relative;
+    z-index:2;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,115 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import axios from 'axios';
+import MessageCard from '@/components/MessageCard.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+      <!-- HEADER -->
+      <div class="header"></div>
+
+      <!-- CONTENT -->
+      <div class="content">
+        <v-container class="content-grid pt-8">
+
+          <!-- MURAL -->
+          <v-row no-gutters class="mb-2">
+            <v-col>
+              <div class="section-header">COMMUNITY MURAL</div>
+            </v-col>
+          </v-row>
+          <v-row no-gutters class="mb-8">
+            <v-col sm="10" offset-sm="1">
+              <div class="section-body mural-container">
+                <img src="https://mumei500k.fromyour.fans/img/mural1.b34eb720.png" class="mural-img">
+                <!-- <img src="@/assets/banner.png" class="mural-img"> -->
+              </div>
+            </v-col>
+          </v-row>
+
+          <!-- MESSAGES -->
+          <v-row no-gutters class="mt-8 mb-4">
+            <v-col>
+              <div class="section-header">MESSAGES</div>
+            </v-col>
+          </v-row>
+          <v-row no-gutters>
+            <v-col>
+              <div class="section-body cards-container">
+                <MessageCard v-for="(item, ix) in cards" :key="`card-${ix}`" :info="item" />
+                <div class="card-clear"></div>
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+    </div>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      source: 'https://vtubertools.sfo3.digitaloceanspaces.com/tribute/mumei500k.json',
+      cards: [],
+    }
+  },
+  mounted() {
+    (async () => {
+      const fetchSource = await axios.get(this.source).catch(() => null);
+      const data = fetchSource && fetchSource.data ? fetchSource.data : { messages: {} };
+      this.cards = Object.values(data.messages).sort((a, b) => a.time - b.time);
+    })();
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width:100%;
+  height:360px;
+  background-image:url("@/assets/banner.png");
+  background-position: -40% center;
+  background-size:auto 115%;
+  background-repeat: repeat-x;
+  background-color:#288485;
+}
+.content {
+  min-height:calc(100vh - 360px);
+  background:linear-gradient(to bottom, #288485 0%, #155455 100%);
+  padding:30px 10px 10px 10px;
+  .content-grid {
+    .section-header {
+      width:94%;
+      margin:0px auto 20px;
+      background:#59b4b6;
+      color:#000000;
+      border-radius:20px;
+      font-size:24px;
+      height:40px;
+      line-height:40px;
+      text-align:center;
+      font-weight:bold;
+    }
+    .section-body {
+      width:90%;
+      margin:0px auto 40px;
+      color:#000000;
+      &.mural-container {
+        .mural-img {
+          width:100%;
+        }
+      }
+      &.cards-container {
+        display: flex;
+        flex-wrap: wrap;
+        .card-clear {
+          clear:both;
+        }
+      }
+    }
+  }
+}
+</style>
